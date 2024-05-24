12ec2bfe73afacb84d60a7f8ca54b0f76ebf7725
Alethepata <alessia.patane@gmail.com>
2024-05-24 16:29:52 +0200

    edit html

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -5,8 +5,20 @@ module.exports = {
         res.format({
             html: () => {
                 let html;
-                posts.forEach(post => html += `<h1>${post.title}</h1>`)
+                posts.forEach(post => {
+                    html += `
+                <div>
+                   <h1>${post.title}</h1>
+                   <img src="${post.image}" alt="${post.title}">
+                   <p>${post.content}</p>
+                   <ul>
+                `;
+                    post.tags.forEach(tag => html += `<li>${tag}</li>`);
+                    html += '</ul></div>';
+                    
+                })
                 res.send(html);
+        
             },
             json: () => {
                 res.json(posts);
